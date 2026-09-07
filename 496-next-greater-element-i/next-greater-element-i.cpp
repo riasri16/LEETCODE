ae56cd7516bf5ead7882104afd22865834f2a276
riya srivastava <168918412+riasri16@users.noreply.github.com>
2026-09-07 23:26:35 +0530

    Time: 0 ms (100.00%) | Memory: 12.8 MB (38.91%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.cpp
+++ 496-next-greater-element-i/next-greater-element-i.cpp
@@ -14,10 +14,6 @@ public:
             if (st.empty())
                 mp[nums2[i]] = -1;
             else{
-                // Chhote/equal elements useless hain
-              while (!st.empty() && st.top() <= nums2[i]) {
-                st.pop();
-              }
                 mp[nums2[i]] = st.top();}
             // Current element ko stack mein daalo
             st.push(nums2[i]);
