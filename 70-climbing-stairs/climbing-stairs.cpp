67328c3b68cf2361dbf805d14343fcd44e6425b8
riya srivastava <168918412+riasri16@users.noreply.github.com>
2026-09-12 00:08:10 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (74.10%) - LeetSync

---
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+       if (n <= 2) return n;
+       int prev2 = 1; // Ways to reach step 1
+       int prev1 = 2; // Ways to reach step 2
+       int current = 0;
+       for(int i=3;i<=n;i++){
+         current = prev1 + prev2; // Add ways from (i-1) and (i-2)
+         prev2 = prev1;           // Shift prev2 forward
+         prev1 = current;         // Shift prev1 forward
+       }
+         
+      return current  ;
+    }
+};
